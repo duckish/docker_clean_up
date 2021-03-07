8270090ec230525a1a6fb2a289957991285cae6d
duckish <nguyen_vanduc@hotmail.com>
2021-03-07 17:36:31 -0500

    Update docker_clean.sh

--- docker_clean.sh
+++ docker_clean.sh
@@ -8,3 +8,9 @@ docker rm $(docker ps -a -q)
 
 ###remove all docker images
 docker rmi $(docker images -a -q) -f
+
+### remove all image network everything
+docker system prune
+
+### remove all volumes
+docker system prune --volumes
